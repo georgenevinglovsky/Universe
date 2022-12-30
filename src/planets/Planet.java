0a85e1db8578f3849b87a909b74a5d5bdd40d8f6
George <georgenevinglovsky@mail.com>
2022-12-29 20:32:20 -0800

    Tasks assigned. Interface created.

--- src/planets/Planet.java
+++ src/planets/Planet.java
@@ -1,4 +1,23 @@
 package planets;
 
-public class Planet {
+public abstract class Planet {
+    private String orbit,planetName;
+    private int quantityOfSatellites;
+    private boolean isPlanetCold, isPlanethot, hasLife, isRound,hasOxygen;
+    private double gravityValue;
+
+    public abstract int solarYear();//method should return q-ty of days for one solar year of this planet
+
+
+
 }
+/*
+Hannan - Venus
+Saiana - Mars
+Hakob - Earth
+George - Sun
+John - Saturn
+Faith - Jupiter
+Robert - Neptune
+Hamid - Uranus
+ */
