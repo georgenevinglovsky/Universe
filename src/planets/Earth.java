8c51ec5e49f8c54dbc16d653ade750921044f10e
Hakob <hakobv68@gmail.com>
2023-01-05 21:36:47 -0800

    changed Earth class

--- src/planets/Earth.java
+++ src/planets/Earth.java
@@ -4,7 +4,7 @@ public class Earth extends Planet{
     public Earth(String orbit, String planetName, int quantityOfSatellites, boolean isPlanetCold, boolean isPlanethot, boolean hasLife, boolean isRound, boolean hasOxygen, double gravityValue) {
         super(orbit, planetName, quantityOfSatellites, isPlanetCold, isPlanethot, hasLife, isRound, hasOxygen, gravityValue);
     }
-
+// Change1
     @Override
     public int solarYear() {
         return 365;
