b306a42dcdf73b1af0eb295fb584318bfe259535
Saiana <saiana.dampilon@gmail.com>
2023-01-05 20:39:40 -0800

    Mars class extended Planet

--- src/planets/Mars.java
+++ src/planets/Mars.java
@@ -1,6 +1,6 @@
 package planets;
 
-public class Mars {
+public class Mars extends Planet {
 
     public static double diameter;
 
@@ -8,10 +8,22 @@ public class Mars {
         diameter = 4212.3;
     }
 
+    public Mars(String orbit, String planetName, int quantityOfSatellites, boolean isPlanetCold, boolean isPlanethot, boolean hasLife, boolean isRound, boolean hasOxygen, double gravityValue) {
+        super(orbit, planetName, quantityOfSatellites, isPlanetCold, isPlanethot, hasLife, isRound, hasOxygen, gravityValue);
+    }
+
     public static void rotate(){
         System.out.println("Mars makes one rotation every 24.6 hours");
     }
 
 
+    @Override
+    public int solarYear() {
+        return 687;
+    }
 
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
