0088a85097e6e4ee002d3ba67c426b1a9392c77e
Saiana <saiana.dampilon@gmail.com>
2023-01-05 17:02:26 -0800

    updated Mars class

--- src/planets/Mars.java
+++ src/planets/Mars.java
@@ -2,7 +2,16 @@ package planets;
 
 public class Mars {
 
-    public double diameter;
+    public static double diameter;
+
+    static{
+        diameter = 4212.3;
+    }
+
+    public static void rotate(){
+        System.out.println("Mars makes one rotation every 24.6 hours");
+    }
+
 
 
 }
