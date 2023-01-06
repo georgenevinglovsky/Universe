59568c193522aec71333bb81a55b211087e0f9eb
cz <canzynl@hotmail.com>
2023-01-05 20:26:05 -0800

    Get orbit update for Saturn

--- src/planets/Saturn.java
+++ src/planets/Saturn.java
@@ -1,6 +1,11 @@
 package planets;
 
 public class Saturn extends Planet{
+    @Override
+    public String getOrbit() {
+        return super.getOrbit();
+    }
+
     public Saturn(String orbit, String planetName, int quantityOfSatellites, boolean isPlanetCold, boolean isPlanethot, boolean hasLife, boolean isRound, boolean hasOxygen, double gravityValue) {
         super(orbit, planetName, quantityOfSatellites, isPlanetCold, isPlanethot, hasLife, isRound, hasOxygen, gravityValue);
     }
