3c648df6de6924d003347849e3fa85cd9eaacb20
FaithSlade <faithwslade@gmail.com>
2023-01-05 20:42:34 -0800

    added implementation of solarYear

--- src/planets/Jupiter.java
+++ src/planets/Jupiter.java
@@ -1,4 +1,13 @@
 package planets;
 
-public class Jupiter{
+public class Jupiter extends Planet{
+
+    public Jupiter(String orbit, String planetName, int quantityOfSatellites, boolean isPlanetCold, boolean isPlanethot, boolean hasLife, boolean isRound, boolean hasOxygen, double gravityValue) {
+        super(orbit, planetName, quantityOfSatellites, isPlanetCold, isPlanethot, hasLife, isRound, hasOxygen, gravityValue);
+    }
+
+    @Override
+    public int solarYear() {
+        return 0;
+    }
 }
